12f93fe16ef3277efc2e6882b0cbd9c05c4d1de6
Farhan Sadid Zihan <101565227+FarhanZihan@users.noreply.github.com>
2023-03-21 14:22:37 +0600

    Add files via upload

---
+++ Practise_Competitive_Programming/Practise_CP_by_C++.cpp
@@ -0,0 +1,20 @@
+// # || Practise DSA || Competitive Programming || C++ ||
+// # || =====\\ Striver's A2Z-DSA Course //===== || Competitive Programming ||
+// # || =====\\ Introduction To Algorithm //===== || Competitive Programming || 
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num;
+
+    cout << "Enter a number :- ";
+    
+    cin >> num;
+
+    cout << "Hello World" << " " << num << endl;
+    
+    return 0;
+}
---
+++ Practise_Competitive_Programming/Practise_CP_by_Python.py
@@ -0,0 +1,4 @@
+# || Practise DSA || Competitive Programming || Python ||
+# || =====\\ Striver's A2Z-DSA Course //===== || Competitive Programming ||
+# || =====\\ Introduction To Algorithm //===== || Competitive Programming || 
+
